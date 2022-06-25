bb94e5e37d9c402bfc36c1db4cda35f4401c44aa
Jason Matta <jt.matta@gmail.com>
2022-06-24 19:37:18 -0700

    add db

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -3,15 +3,13 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   //user: process.env.PGUSERNAME,
-  host: "127.0.0.1",
-  database: "bark",
-  //password: process.env.PGPASS,
-  port: 5432,
+  host: process.env.PGHOST || "127.0.0.1",
+  database: process.env.PGDATABASE || "bark",
+  //password: process.env.PGPASS || "",
+  port: process.env.PGPORT || 5432,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 })
-
-console.log(process.env.PGPORT)
 
 const db = Promise.promisifyAll(pool, {multiArgs: true});
 
