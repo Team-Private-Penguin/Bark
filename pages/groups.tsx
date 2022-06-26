d5468f542dd0af2f0287d8104a52bc3420bf91ee
Jason Matta <jt.matta@gmail.com>
2022-06-26 16:40:23 -0700

    refactor EventCard

--- pages/groups.tsx
+++ pages/groups.tsx
@@ -9,24 +9,26 @@ const Groups = () => {
   return (
     <main className="min-h-screen w-screen p-5">
       <Group className="Group" position="apart">
-        <Stack className="hidden lg:flex" style={{ width: "20%" }}>
+        <Stack className="hidden lg:flex sticky" style={{ width: "19%" }}>
           <div className="border h-[33vh] ">
             <h2>🐶 User Info</h2>
           </div>
-          <div className="border h-[60vh]">
+          <div className="border h-[59vh]">
             <h2>🐶 Groups</h2>
             <GroupsComp />
           </div>
         </Stack>
         <Stack style={{ flexGrow: "1" }}>
-          <div className="border flex-column h-[95vh]">
-            <h2>🐶 Feed</h2>
-            <AddEvent />
+          <div className="border flex-column h-[94vh] overflow-auto">
+            <div className="sticky top-0 z-50">
+              <h2>🐶 Feed</h2>
+              <AddEvent />
+            </div>
             <EventFeed />
           </div>
         </Stack>
-        <Stack className="hidden xl:flex" style={{ width: "20%" }}>
-          <div className="border h-[95vh]">
+        <Stack className="hidden xl:flex" style={{ width: "19%" }}>
+          <div className="border h-[94vh]">
             <h2>🐶 Group Members</h2>
           </div>
         </Stack>
