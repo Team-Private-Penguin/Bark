0d5afe066b045f2fef823d66c324689da7a77bf1
Jason Matta <jt.matta@gmail.com>
2022-06-26 13:01:51 -0700

    refactor addpost

--- pages/groups.tsx
+++ pages/groups.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Post from "../components/Post";
-import axios from "axios";
 import {
   Button,
   Checkbox,
@@ -16,51 +15,12 @@ import {
 import { DatePicker } from "@mantine/dates";
 import Link from "next/link";
 import { useState } from "react";
-import { stateData } from "./statedata.js";
+import { stateData } from "../components/statedata.js";
 import { useForm } from "@mantine/form";
+import AddPost from "../components/AddPost";
+import GroupsComp from "../components/Groups";
 
 const Groups = () => {
-  const [opened, setOpened] = useState(false);
-  const form = useForm({
-    initialValues: {
-      eventName: "",
-      address_1: "",
-      address_2: "",
-      city: "",
-      state: "",
-      zipcode: "",
-      description: "",
-      date: "",
-      prospective: false,
-      img_url: "",
-    },
-  });
-
-  const submitEvent = (values: {
-    eventName: string;
-    address_1: string;
-    address_2: string;
-    city: string;
-    date: Date;
-    description: string;
-    prospective: boolean;
-    state: string;
-    zipcode: string;
-    img_url: string;
-  }) => {
-    const submission = {
-      group_id: 1,
-      eventName: values.eventName,
-      address: `${values.address_1}${
-        values.address_2 ? values.address_2 : ""
-      }, ${values.city}, ${values.state} ${values.zipcode}`,
-      date: values.date,
-      description: values.description,
-      prospective: values.prospective,
-    };
-    axios.post("./api/d/?group_id=1", submission);
-    console.log("big moves", submission);
-  };
   return (
     <main className="min-h-screen w-screen p-5">
       <Group className="Group" position="apart">
@@ -70,89 +30,15 @@ const Groups = () => {
           </div>
           <div className="border h-[60vh]">
             <h2>🐶 Groups</h2>
+            <GroupsComp />
           </div>
         </Stack>
         <Stack style={{ flexGrow: "1" }}>
           <div className="border flex-column h-[95vh]">
             <h2>🐶 Feed</h2>
-            <Post />
-            <Modal
-              opened={opened}
-              onClose={() => setOpened(false)}
-              title="Create New Event!"
-            >
-              <form onSubmit={form.onSubmit((values) => submitEvent(values))}>
-                <TextInput
-                  placeholder="Event Name"
-                  label="Event name"
-                  required
-                  {...form.getInputProps("eventName")}
-                />
-                <TextInput
-                  placeholder="Address Line 1"
-                  label="Address Line 1"
-                  required
-                  {...form.getInputProps("address_1")}
-                />
-                <TextInput
-                  placeholder="Address Line 2"
-                  label="Address Line 2"
-                  {...form.getInputProps("address_2")}
-                />
-                <TextInput
-                  placeholder="City"
-                  label="City"
-                  required
-                  {...form.getInputProps("city")}
-                />
-                <Select
-                  label="State"
-                  placeholder="Select one"
-                  searchable
-                  nothingFound="No options"
-                  maxDropdownHeight={280}
-                  data={stateData}
-                  {...form.getInputProps("state")}
-                />
-                <TextInput
-                  placeholder="Zip Code"
-                  label="Zip Code"
-                  required
-                  {...form.getInputProps("zipcode")}
-                />
-                <Textarea
-                  placeholder="Description"
-                  label="Description"
-                  required
-                  {...form.getInputProps("description")}
-                />
-                <DatePicker
-                  placeholder="Pick date"
-                  label="Event date"
-                  required
-                  {...form.getInputProps("date")}
-                />
-                <Checkbox
-                  label="Prospective?"
-                  {...form.getInputProps("prospective")}
-                />
-                <Group position="right" mt="md">
-                  <Button color="dark" type="submit">
-                    Submit
-                  </Button>
-                </Group>
-              </form>
-            </Modal>
-
-            <Group position="center">
-              <Button
-                variant="white"
-                className="text-black w-auto p-2"
-                onClick={() => setOpened(true)}
-              >
-                Open Modal
-              </Button>
-            </Group>
+            <AddPost />
+            <Post image={true} />
+            <Post image={false} />
           </div>
         </Stack>
         <Stack className="hidden xl:flex" style={{ width: "20%" }}>
