25b985d5fbdfc98a1cb9a39f12719ad7747dc153
Jason Matta <jt.matta@gmail.com>
2022-06-28 20:51:21 -0700

    add groupwork

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,5 +1,11 @@
 import db from "./db";
-import { postEvent, getEventsGroup, getAdmin, deleteEvent, updateEvent } from "./db/model";
+import {
+  postEvent,
+  getEventsGroup,
+  getAdmin,
+  deleteEvent,
+  updateEvent,
+} from "./db/model";
 
 export default function handler(req, res) {
   if (req.method === "POST") {
@@ -17,44 +23,52 @@ export default function handler(req, res) {
         console.log(err);
         res.status(404).send(err);
       });
-  } else if (req.method === 'GET' && req.query.body === 'admin') { //for getting admin id.
+  } else if (req.method === "GET" && req.query.body === "admin") {
+    //for getting admin id.
 
     return db
-    .queryAsync(getAdmin)
-    .then(() => res.status(200).send("OK")) //this need to change to send the admin id back.
-    .catch((err) => {
-      res.status(404).send(err);
-    })
-
+      .queryAsync(getAdmin)
+      .then(() => res.status(200).send("OK")) //this need to change to send the admin id back.
+      .catch((err) => {
+        console.log(err);
+        res.status(404).send(err);
+      });
   } else if (req.method === "GET") {
-    let { group_id } = req.body;
+    let { group_id } = req.query;
     const query = {
       text: getEventsGroup,
       values: [group_id],
     };
+    console.log(query);
     return db
       .queryAsync(query)
       .then((results) => res.status(200).send(results))
-      .catch((err) => res.status(404).send(err));
+      .catch((err) => {
+        console.log(err);
+        res.status(404).send(err);
+      });
   } else if (req.method === "DELETE") {
     return db
-    .queryAsync(deleteEvent, req.query.body)
-    .then(() => {
-      res.status(200).send("Deleted!");
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    })
-  } else if (req.method === "PUT") {  //consider patch
+      .queryAsync(deleteEvent, req.query.body)
+      .then(() => {
+        res.status(200).send("Deleted!");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send(err);
+      });
+  } else if (req.method === "PUT") {
+    //consider patch
 
     return db
-    .queryAsync(updateEvent, req.body)
-    .then(() => {
-      res.status(200).send("Updated!");
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    })
+      .queryAsync(updateEvent, req.body)
+      .then(() => {
+        res.status(200).send("Updated!");
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send(err);
+      });
   } else {
     res.sendStatus(404);
   }
