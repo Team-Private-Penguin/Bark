e67e7e3a8aa171bfd721e19568a676436bfbd90b
Jason Matta <jt.matta@gmail.com>
2022-06-30 03:30:53 -0700

    refactor event detail

--- components/EventDetail.js
+++ components/EventDetail.js
@@ -12,6 +12,7 @@ import {
   Badge,
   Title,
   Button,
+  ScrollArea,
 } from "@mantine/core";
 import axios from "axios";
 
@@ -32,7 +33,6 @@ function EventDetail({
     prospective,
     description,
     group_name,
-    admin_id,
     event_id,
   } = event;
   const timeStamp = new Date(date);
@@ -75,7 +75,7 @@ function EventDetail({
   return (
     <div className="flex w-full h-full items-top justify-center space-x-2">
       <Card
-        className="sticky top-0 space-y-1 w-[74%]"
+        className="sticky top-0 space-y-2 w-[74%]"
         radius="10px"
         shadow="sm"
       >
@@ -102,9 +102,12 @@ function EventDetail({
           ) : null}
           {prospective ? <Badge color="grape">PLANNING EVENT</Badge> : null}
         </Stack>
-        <Card.Section className="flex items-center justify-center space-x-4">
-          <Stack className="w-[40%]">
-            <Image radius="10px" fit="contain" src={image} />
+        <Card.Section className="h-[25vh] flex items-center justify-center space-x-4">
+          <Stack>
+            <img
+              className="rounded-[10px] max-h-[25vh] max-w-[27vw]"
+              src={image}
+            />
           </Stack>
         </Card.Section>
 
@@ -117,9 +120,9 @@ function EventDetail({
               label={prospective ? "Interested?" : "RSVP"}
             ></Switch>
           </Group>
-          <Text color="var(--black)" size="lg">
+          <Title order={5} color="var(--black)" size="lg">
             {address}
-          </Text>
+          </Title>
           <Text color="var(--black)" size="md">
             {description}
           </Text>
@@ -131,7 +134,11 @@ function EventDetail({
             setComments={setComments}
           />
         </Card.Section>
-        <Card.Section className="p-2 h-[37vh] overflow-auto">
+        <Card.Section
+          className={`p-2 ${
+            prospective ? "h-[25vh]" : "h-[29vh]"
+          } overflow-auto`}
+        >
           <CommentFeed
             comments={comments}
             isOwner={canEdit}
@@ -139,13 +146,15 @@ function EventDetail({
           />
         </Card.Section>
       </Card>
-      <Card className="space-y-1 w-[24%] h-[85vh]" radius="10px" shadow="sm">
+      <Card className="space-y-1 w-[24%] h-[90vh]" radius="10px" shadow="sm">
         <Card.Section className="bg-main p-2">
-          <h3 className="text-left font-bold color-accent">Attendees</h3>
+          <Title align="center" order={4}>
+            Attendees
+          </Title>
         </Card.Section>
-        <Card.Section className="h-[95%] overflow-auto space-y-72">
+        <ScrollArea className="h-[98%] overflow-auto">
           {attendeeList}
-        </Card.Section>
+        </ScrollArea>
       </Card>
     </div>
   );
