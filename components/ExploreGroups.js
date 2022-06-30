995d965ad0b03ba70d6d99af3e71e2e7cca591ad
Jason Matta <jt.matta@gmail.com>
2022-06-30 14:02:05 -0700

    add css change to explore groups

--- components/ExploreGroups.js
+++ components/ExploreGroups.js
@@ -42,19 +42,24 @@ const ExploreGroups = () => {
         onClose={() => setOpened(false)}
         title="Find New Groups!"
         className="font-bold"
+        size="lg"
       >
         {allGroups.map((group, index) => (
           <Link key={index} href={`/group?id=${group.group_id}`} passHref>
             <Card
               radius="md"
               p="md"
-              className="cursor-pointer bg-white text-black mt-2 drop-shadow-md hover:drop-shadow-sm hover:text-teal"
+              className="w-full cursor-pointer bg-white text-black mt-2 drop-shadow-lg hover:drop-shadow-md hover:text-teal"
               onClick={() => setOpened(false)}
             >
               <Group>
-                <FontAwesomeIcon className="" icon={faPaw} />
-                <Title order={4}> {group.name}</Title>
-                <Text className="text-xs">{group.description}</Text>
+                <FontAwesomeIcon className="paw-groups" icon={faPaw} />
+                <div>
+                  <Title order={3}> {group.name}</Title>
+                  <Text className="text-xs font-normal">
+                    {group.description}
+                  </Text>
+                </div>
               </Group>
             </Card>
           </Link>
