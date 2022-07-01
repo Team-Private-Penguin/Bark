ea4d2bba7acb75fcf38fa0dbb96f6a83ddf18bd7
Jason Matta <jt.matta@gmail.com>
2022-07-01 14:16:09 -0700

    change db timeout to 10sec

--- pages/api/db/index.js
+++ pages/api/db/index.js
@@ -1,17 +1,16 @@
 const Promise = require("bluebird");
 const { Pool } = require("pg");
 const { FileWatcherEventKind } = require("typescript");
-require('dotenv').config();
+require("dotenv").config();
 
 const pool = new Pool({
-
   user: process.env.PGUSERNAME,
   host: process.env.PGHOST,
   database: "bark",
   password: process.env.PGPASS,
   port: process.env.PGPORT,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
 });
 
 const db = Promise.promisifyAll(pool, { multiArgs: true });
