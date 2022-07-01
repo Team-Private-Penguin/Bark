9202914953bd20e952012ca2342afbf516938ff6
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-07-01 11:01:38 -0600

    Title of Diologue in MapDrawerCard now links to the group page

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
 import { GoogleMap, Marker, LoadScript } from '@react-google-maps/api';
 import { Drawer, Button, Group, Stack, TextInput, Alert } from '@mantine/core';
 import { MapContainerState } from './MapContainer.jsx';
@@ -35,7 +35,7 @@ export default function Map() {
 
         searchResults.map((result) => {
           if (result.img_url === '') {
-            console.log(result);
+            //console.log(result);
             result.img_url = 'https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png';
           }
         })
